b9fd6c9b6afa05309487dd32598791244ef470bb
Nobsmoke123 <donaldakobundu@gmail.com>
2020-09-21 22:20:07 +0100

    wiring the sign up page

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -8,9 +8,23 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+
+    bool isLoading = false;
+
+    final formKey = GlobalKey<FormState>();
+
     TextEditingController usernameController = new TextEditingController();
     TextEditingController emailController = new TextEditingController();
     TextEditingController passwordController = new TextEditingController();
+
+    void signUp(){
+//        Validate the form fields
+        if(formKey.currentState.validate()){
+            setState(() {
+              isLoading = true;
+            });
+        }
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +43,10 @@ class _SignUpState extends State<SignUp> {
                         crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
                             Form(
+                                key: formKey,
                               child: Column(
                                 children: [
-                                    TextField(
+                                    TextFormField(
                                         style: TextStyle(
                                             color: Colors.white,
                                         ),
@@ -48,9 +63,12 @@ class _SignUpState extends State<SignUp> {
                                             ),
                                         ),
                                         controller: usernameController,
+                                        validator: (val){
+                                            return val.isEmpty || val.length < 2 ? "Please provide username greater" : null;
+                                        },
                                     ),
 
-                                    TextField(
+                                    TextFormField(
                                         style: TextStyle(
                                             color: Colors.white,
                                         ),
@@ -67,9 +85,13 @@ class _SignUpState extends State<SignUp> {
                                             ),
                                         ),
                                         controller: emailController,
+                                        validator: (val){
+                                            return RegExp(r"^[a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ? null :  "Please provide a valid email";
+                                        },
                                     ),
 
-                                    TextField(
+                                    TextFormField(
+                                        obscureText: true,
                                         style: TextStyle(
                                             color: Colors.white,
                                         ),
@@ -86,6 +108,9 @@ class _SignUpState extends State<SignUp> {
                                             ),
                                         ),
                                         controller: passwordController,
+                                        validator: (val){
+                                            return val.length < 6 ? "Password length must be greater than 5" : null;
+                                        },
                                     ),
                                 ],
                               ),
@@ -93,50 +118,59 @@ class _SignUpState extends State<SignUp> {
 
                             SizedBox(height: 16),
 
-                            Container(
-                                alignment: Alignment.centerRight,
-                                child: Container(
-                                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                                    child: Text("Forgot Password", style: TextStyle(color: Colors.white),),
-                                ),
+                            GestureDetector(
+                                onTap: (){},
+                                  child: Container(
+                                      alignment: Alignment.centerRight,
+                                      child: Container(
+                                          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                                          child: Text("Forgot Password", style: TextStyle(color: Colors.white),),
+                                      ),
+                                  ),
                             ),
 
                             SizedBox(height: 16,),
 
-                            Container(
-                                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                                decoration: BoxDecoration(
-                                    color: Colors.blue,
-                                    borderRadius: BorderRadius.all(Radius.circular(50))
-                                ),
-                                child: Text(
-                                    "Sign Up",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600,
-                                    ),
-                                ),
+                            GestureDetector(
+                                onTap: (){},
+                              child: Container(
+                                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                                  decoration: BoxDecoration(
+                                      color: Colors.blue,
+                                      borderRadius: BorderRadius.all(Radius.circular(50))
+                                  ),
+                                  child: Text(
+                                      "Sign Up",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600,
+                                      ),
+                                  ),
+                              ),
                             ),
 
                             SizedBox(height: 16,),
 
-                            Container(
-                                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.all(Radius.circular(50))
-                                ),
-                                child: Text(
-                                    "Sign Up with Google",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        color: Colors.black45,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600,
-                                    ),
-                                ),
+                            GestureDetector(
+                                onTap: () {},
+                              child: Container(
+                                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.all(Radius.circular(50))
+                                  ),
+                                  child: Text(
+                                      "Sign Up with Google",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.black45,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600,
+                                      ),
+                                  ),
+                              ),
                             ),
 
                             SizedBox(height: 16,),
@@ -151,14 +185,17 @@ class _SignUpState extends State<SignUp> {
                                         ),
                                     ),
 
-                                    Text(
-                                        "Login now",
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            decoration: TextDecoration.underline,
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w600,
-                                        ),
+                                    GestureDetector(
+                                        onTap: (){},
+                                      child: Text(
+                                          "Login now",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              decoration: TextDecoration.underline,
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w600,
+                                          ),
+                                      ),
                                     ),
                                 ],
                             ),
