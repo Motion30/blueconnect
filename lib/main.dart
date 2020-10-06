e1f569195326c9ba95588ae648c47fdadad0a6fc
Nobsmoke123 <donaldakobundu@gmail.com>
2020-10-06 06:09:01 -0700

    restructuring the app architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,15 @@
-import 'screens/add_community.dart';
-import 'screens/add_groups.dart';
-import 'screens/select.dart';
-import 'providers/user.dart';
-import 'screens/authenticate.dart';
-import 'screens/search.dart';
-import 'screens/conversation.dart';
-import 'screens/forget_password.dart';
-import 'screens/home.dart';
-import 'screens/channel.dart';
-import 'screens/profile.dart';
-import 'screens/settings.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/sign_in.dart';
-import 'screens/sign_up.dart';
-import 'utils/color.dart';
-import 'package:provider/provider.dart';
-import 'providers/auth_data.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (ctx) => UserProvider(),),
-      ],
-      child: MaterialApp(
+    return MaterialApp(
             title: 'Blue Connect',
             debugShowCheckedModeBanner: false,
             theme: ThemeData(
@@ -39,25 +17,7 @@ class MyApp extends StatelessWidget {
               scaffoldBackgroundColor: KBackgroundColor,
               visualDensity: VisualDensity.adaptivePlatformDensity,
             ),
-
             home: Authenticate(),
-
-            routes: {
-              Authenticate.screenId : (context) => Authenticate(),
-              SignIn.screenId : (context) => SignIn(),
-              Home.screenId: (context) => Home(),
-              SignUp.screenId : (context) => SignUp(),
-              Profile.screenId: (context) => Profile(),
-              Channel.screenId: (context) => Channel(),
-              Settings.screenId: (context) => Settings(),
-              ForgetPassword.screenId: (context) => ForgetPassword(),
-              Conversation.screenId: (context) => Conversation(),
-              Select.screenId: (context) => Select(),
-              AddGroup.screenId: (context) => AddGroup(),
-              AddCommunity.screenId: (context) => AddCommunity(),
-                Search.screenId : (context) => Search(),
-            },
-          )
     );
   }
 }
--- lib/utils/color.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-//const KBackgroundColor = Color(0xFF1F1F1F);
-const KBackgroundColor = Color(0xFF89CFF0);
-const KAppbarColor = Color(0xFF2196F3);
-const KPrimaryColor = Color(0xFF);
