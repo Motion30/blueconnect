e78e0d9cc4994b9150333c35da8d91d17886b605
Nobsmoke123 <donaldakobundu@gmail.com>
2020-10-09 19:18:30 -0700

    added the router routes

--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -5,7 +5,31 @@ import 'package:blueconnectapp/core/constants/route_paths.dart';
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings){
     switch(settings.name){
-      case '/' :
+      case Routes.INDEX_SCREEN :
+        return MaterialPageRoute(builder: (_) => null);
+
+      case Routes.START_UP_SCREEN:
+        return MaterialPageRoute(builder: (_) => null);
+
+      case Routes.LOGIN_SCREEN:
+        return MaterialPageRoute(builder: (_) => null);
+
+      case Routes.REGISTER_SCREEN:
+        return MaterialPageRoute(builder: (_) => null);
+
+      case Routes.FORGOT_PASSWORD_SCREEN:
+        return MaterialPageRoute(builder: (_) => null);
+
+      case Routes.VERIFICATION_SCREEN:
+        return MaterialPageRoute(builder: (_) => null);
+
+      case Routes.HOME_SCREEN:
+        return MaterialPageRoute(builder: (_) => null);
+
+      case Routes.CREATE_SCREEN:
+        return MaterialPageRoute(builder: (_) => null);
+
+      case Routes.ADD_GROUP_SCREEN:
         return MaterialPageRoute(builder: (_) => null);
 
       default:
